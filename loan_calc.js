6151aff516e22161087cb4a34c3d953250eaa84f
Prisha <prishamukherjee2015@gmail.com>
2024-06-29 13:26:43 +0530

    First commit

---
+++ loan_calc.js
@@ -0,0 +1,64 @@
+const loanAmountInput = document.querySelector(".loan-amount");
+const interestRateInput = document.querySelector(".interest-rate");
+const loanTenureInput = document.querySelector(".loan-tenure");
+
+const loanEMIValue = document.querySelector(".loan-emi .value");
+const totalInterestValue = document.querySelector(".total-interest .value");
+const totalAmountValue = document.querySelector(".total-amount .value");
+
+const calculateBtn = document.querySelector(".calculate-btn");
+
+let loanAmount, interestRate, loanTenure, interest;
+let myChart;
+
+const displayChart = (totalInterestPayableValue) => {
+  const ctx = document.getElementById("myChart").getContext("2d");
+  myChart = new Chart(ctx, {
+    type: "pie",
+    data: {
+      labels: ["Total Interest", "Principal Loan Amount"],
+      datasets: [{
+        data: [totalInterestPayableValue, loanAmount],
+        backgroundColor: ["rgb(150,0,0)", "rgb(0,0,130)"],
+        borderWidth: 0,
+      }],
+    },
+  });
+};
+
+const updateChart = (totalInterestPayableValue) => {
+  myChart.data.datasets[0].data[0] = totalInterestPayableValue;
+  myChart.data.datasets[0].data[1] = loanAmount;
+  myChart.update();
+};
+
+const calculateEMIAndUpdate = () => {
+  loanAmount = parseFloat(loanAmountInput.value);
+  interestRate = parseFloat(interestRateInput.value);
+  loanTenure = parseFloat(loanTenureInput.value);
+  interest = interestRate / 12 / 100;
+
+  let emi = loanAmount * interest * (Math.pow(1 + interest, loanTenure) /
+      (Math.pow(1 + interest, loanTenure) - 1));
+
+  loanEMIValue.innerHTML = Math.round(emi);
+
+  let totalAmount = Math.round(loanTenure * emi);
+  totalAmountValue.innerHTML = totalAmount;
+
+  let totalInterestPayable = Math.round(totalAmount - loanAmount);
+  totalInterestValue.innerHTML = totalInterestPayable;
+
+  if (myChart) {
+    updateChart(totalInterestPayable);
+  } else {
+    displayChart(totalInterestPayable);
+  }
+};
+
+const init = () => {
+  calculateEMIAndUpdate();
+};
+
+init();
+calculateBtn.addEventListener("click", init);
